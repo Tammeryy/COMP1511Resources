6ae385ee92ec671605d96752413bd320b861f829
Tammy <TammyXQZ@yahoo.com>
2020-08-10 12:24:41 +1000

    tut

--- countDuplicates.c
+++ countDuplicates.c
@@ -7,15 +7,36 @@ int countDuplicates(int arr[SIZE], int length, int value);
 
 int main(void) {
     int arr[SIZE];
-    // scan in input from user until ctrl d
-    // count duplicates
     
-
+    int counter = 0;
+    // scan in input from user until ctrl d
+    while (scanf("%d", &arr[counter]) == 1) { 
+        counter++;
+    }
+    // counter == length of our array that's initialised
+    
+    
+    // count duplicates
+    int duplicates = countDuplicates(arr, counter, 10); // count the number of times 10 appears
+    
+    printf("The number 10 appeared %d times\n", duplicates);
+    
     return 0;
 }
 
 // count how many duplicates are there of a particular value
 // return the number of duplicates
 int countDuplicates(int arr[SIZE], int length, int value) {
-    return 0;
+    int valCount = 0; 
+    int counter = 0;
+    
+    while (counter < length) {
+        
+        if (arr[counter] == value) {
+            valCount++;
+        }
+        counter++; 
+    }
+    return valCount;
+
 }
